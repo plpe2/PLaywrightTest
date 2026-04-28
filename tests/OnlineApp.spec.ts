9ad585dd56ed6542277583db84f5d30edfa20fd8
plpe2 <villanuevapv1@gmail.com>
2026-04-28 10:55:06 +0800

    Feature: Implement BpApplication and RegistrationPage classes with associated methods

--- tests/OnlineApp.spec.ts
+++ tests/OnlineApp.spec.ts
@@ -1,5 +1,18 @@
 import { test, expect } from "@playwright/test";
+import { BpApplication } from "../pages/Online Application/BP Application/bpApplication.page";
 
 test("BpApplication", async ({ page }) => {
-  await page.goto("http://192.168.20.71:1024/Account/Login?statusCode=0");
+  var BpApp = new BpApplication({
+    page,
+    passedValues: {
+      userNameValue: "RCAMBAL",
+      securityCodeValue: "0000025",
+    },
+  });
+
+  await BpApp.gotoApplication();
+  await BpApp.loginAccount();
+  await BpApp.otpCode();
+
+  // await page.goto("http://192.168.20.71:1024/Account/Login?statusCode=0");
 });
--- tests/Registration.spec.ts
+++ tests/Registration.spec.ts
@@ -1,21 +1,21 @@
 import { test, expect } from "@playwright/test";
-import { RegistrationPage } from "../pages/Registration/registration.page";
+import { RegistrationPage } from "../pages//Online Application/Registration/registration.page";
 
 test("Registration", async ({ page }) => {
   var Rp = new RegistrationPage({
     page: page,
     urlLink: "http://192.168.20.71:1024/",
     OwnerInfo: {
-      firstName: "france",
-      lastName: "ronald",
+      firstName: "donald",
+      lastName: "hugh",
     },
     ContactInfo: {
-      email: "fronald@example.com",
+      email: "dhugh@example.com",
       mobileNumber: "09155452264",
       address: "1243 Fleet Street",
       zipCode: "4102",
     },
-    usernameValue: "fronald",
+    usernameValue: "dhugh",
     passwordValue: "P@ssw0rd",
   });
 
