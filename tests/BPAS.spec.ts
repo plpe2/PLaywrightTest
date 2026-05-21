6611ac75776225b6598e74d09f17bc9efb2a0fda
plpe2 <villanuevapv1@gmail.com>
2026-05-21 11:16:59 +0800

    feat: Electrical evaluation process

--- tests/BPAS.spec.ts
+++ tests/BPAS.spec.ts
@@ -2,20 +2,26 @@ import test from "@playwright/test";
 import { BPASHelper } from "../helpers/BPAS/BPASHelper.helpers";
 import { Architectural } from "../pages/BPAS/Evaluation/Architectural.page";
 import { Geodetic } from "../pages/BPAS/Evaluation/Geodetic.page";
+import { Electrical } from "../pages/BPAS/Evaluation/Electrical.page";
 
 test("Architectural Evaluation", async ({ page }) => {
-  var helpers = new BPASHelper(page);
   var archiTest = new Architectural(page);
 
-  await helpers.loginBPAS();
+  await archiTest.loginBPAS();
   await archiTest.evaluationProcess();
   await page.locator("#btnSaveBldgEval").click();
 });
 
 test("Geodetic Evaluation", async ({ page }) => {
-  var helpers = new BPASHelper(page);
   var geoTest = new Geodetic(page);
 
-  await helpers.loginBPAS();
+  await geoTest.loginBPAS();
   await geoTest.evaluationProcess();
+});
+
+test("Electrical Evaluation", async ({ page }) => {
+  var elecTest = new Electrical(page);
+
+  await elecTest.loginBPAS();
+  await elecTest.evaluationProcess();
 });
