4e892a057c45c06ce82977bbdf0b49683156fbff
plpe2 <villanuevapv1@gmail.com>
2026-05-25 15:29:37 +0800

    feat: Encoding of findings Remarks on Inspection Module

--- tests/Inspection.spec.ts
+++ tests/Inspection.spec.ts
@@ -1,9 +1,17 @@
 import test from "@playwright/test";
 import { MissionOrder } from "../pages/BPAS/Inspection/MissionOrder.page";
+import { Findings } from "../pages/BPAS/Inspection/Findings";
 
 test("Generate Mission Order", async ({ page }) => {
   var InspectionMO = new MissionOrder({ page: page, testEnvironment: false });
 
   await InspectionMO.loginBPAS();
   await InspectionMO.GenerateMissionOrder("NBP2605-00008");
+});
+
+test("Encoding Findings Remarks", async ({ page }) => {
+  var FindingsTab = new Findings({ page: page, testEnvironment: false });
+
+  await FindingsTab.loginBPAS();
+  await FindingsTab.EncodeRemarks("NBP2605-00008");
 });
