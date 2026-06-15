e7b6feb8e8badef7ba89a28561d452e3146ba4c8
plpe2 <villanuevapv1@gmail.com>
2026-06-15 16:20:57 +0800

    feat: added BPAS inspection for wholeProcess

--- tests/WholeProces.spec.ts
+++ tests/WholeProces.spec.ts
@@ -2,14 +2,25 @@ import test from "@playwright/test";
 import { ReceiveApp } from "../pages/WebPortal/ReceiveApp.pages";
 import { RefactoredReceiving } from "../pages/PTRAX/RefactoredReceving.pages";
 import { ProcessHandler } from "../helpers/PTRAX/ProcessHandler.helpers";
+import { MissionOrder } from "../pages/BPAS/Inspection/MissionOrder.page";
+import { Findings } from "../pages/BPAS/Inspection/Findings";
 
 test("Receiving to Releasing", async ({ page }) => {
   // global value for Application Number
-  const AppNumber = "NBP2606-00003";
+  const AppNumber = "NBP2606-00014";
+  const isTestEnvironment: boolean = false;
   // Initialization of classes that will be used in this test
-  var WebPortal = new ReceiveApp({ page: page, testEnvironment: true });
+  var WebPortal = new ReceiveApp({
+    page: page,
+    testEnvironment: isTestEnvironment,
+  });
   var PTRAXProcess = new RefactoredReceiving(page);
-  var PTRAXLogin = new ProcessHandler(page);
+  var PTRAXLogin = new ProcessHandler({
+    page: page,
+    testEnvironment: isTestEnvironment,
+  });
+  var InspectionMO = new MissionOrder({ page: page, testEnvironment: false });
+  var FindingsTab = new Findings({ page: page, testEnvironment: false });
 
   //Receving
   await WebPortal.loginWebPortal();
@@ -19,8 +30,13 @@ test("Receiving to Releasing", async ({ page }) => {
   await PTRAXLogin.loginAcc("receiving");
   await PTRAXProcess.ReceiveApp(AppNumber);
   await PTRAXProcess.JumpApp(AppNumber, await PTRAXLogin.jumpSteps[3]);
+  await PTRAXLogin.logout();
   await PTRAXLogin.loginAcc("siteverification");
   await PTRAXProcess.ReceiveApp(AppNumber);
+
+  await InspectionMO.loginBPAS();
+  await InspectionMO.GenerateMissionOrder(AppNumber);
+  await FindingsTab.EncodeRemarks(AppNumber);
 
   //Evaluation
 
